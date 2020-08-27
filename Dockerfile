34a309fe37316b984f982e3065ab7a3664b2ed6e
mhycy <i@mhycy.me>
2020-08-27 17:22:12 +0800

    feat: nginx 1.18.0 with openssl 1.1.1g, zlib 1.2.11, pcre 8.44

---
+++ Dockerfile
@@ -0,0 +1,100 @@
+FROM ubuntu:18.04 AS nginx-build
+
+WORKDIR /build
+
+ENV NGINX_VERSION     "1.18.0"
+ENV OPENSSL_VERSION   "1.1.1g"
+ENV PCRE_VERSION      "8.44"
+ENV ZLIB_VERSION      "1.2.11"
+
+ENV BUILD_ROOT        "/build"
+ENV BUILD_SRC         "/build/src"
+ENV BUILD_RELEASE     "/build/release"
+ENV NGINX_USER        "nginx"
+ENV NGINX_GROUP       "nginx"
+ENV NGINX_PREFIX      "/etc/nginx"
+ENV NGINX_SBIN_PATH   "/usr/sbin/nginx"
+ENV NGINX_CONF_PATH   "/etc/nginx/nginx.conf"
+ENV NGINX_ERROR_LOG_PATH  "stderr"
+ENV NGINX_HTTP_LOG_PATH   "/dev/stdout"
+ENV NGINX_PID_PATH    "/var/run/nginx.pid"
+ENV NGINX_LOCK_PATH   "/var/run/nginx.lock"
+
+RUN apt update \
+    && apt install -y git wget build-essential autoconf libtool automake golang cmake \
+    && rm -rf ${BUILD_SRC} \
+    && mkdir -p ${BUILD_SRC} \
+    && mkdir -p ${BUILD_RELEASE} \
+    && cd ${BUILD_SRC} \
+    && wget -O- https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz | tar xz \
+    && wget -O- https://ftp.pcre.org/pub/pcre/pcre-${PCRE_VERSION}.tar.gz | tar xz \
+    && wget -O- https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz | tar xz \
+    # ngx_brotli & libbrotli
+    && git clone https://github.com/google/ngx_brotli \
+    && cd ngx_brotli \
+    && git submodule update --init \
+    && cd ${BUILD_SRC} \
+    && wget -O- http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz | tar xz \
+    && cd nginx-${NGINX_VERSION} \
+    && ./configure \
+        --add-module=../ngx_brotli \
+        --with-openssl=../openssl-${OPENSSL_VERSION} \
+        --with-openssl-opt='enable-tls1_3 enable-weak-ssl-ciphers' \
+        --with-threads \
+        --with-select_module \
+        --with-poll_module \
+        --with-pcre=../pcre-${PCRE_VERSION} \
+        --with-zlib=../zlib-${ZLIB_VERSION} \
+        --with-pcre-jit \
+        --with-http_v2_module \
+        --with-http_ssl_module \
+        --with-http_gunzip_module \
+        --with-http_gzip_static_module \
+        --with-stream  \
+        --with-stream_ssl_module \
+        --with-stream_ssl_preread_module \
+        --with-http_realip_module \
+        --with-http_auth_request_module \
+        --with-debug \
+        --user=${NGINX_USER} \
+        --group=${NGINX_GROUP} \
+        --prefix=${NGINX_PREFIX} \
+        --sbin-path=${NGINX_SBIN_PATH} \
+        --conf-path=${NGINX_CONF_PATH} \
+        --error-log-path=${NGINX_ERROR_LOG_PATH} \
+        --http-log-path=${NGINX_HTTP_LOG_PATH} \
+        --pid-path=${NGINX_PID_PATH} \
+        --lock-path=${NGINX_LOCK_PATH} \
+    && make \
+    && cd ${BUILD_SRC} \
+    && mkdir -p ${BUILD_RELEASE}${NGINX_PREFIX}/conf.d \
+    && cp nginx-${NGINX_VERSION}/objs/nginx ${BUILD_RELEASE} \
+    && cp nginx-${NGINX_VERSION}/conf/* ${BUILD_RELEASE}${NGINX_PREFIX} \
+
+FROM ubuntu:18.04
+
+COPY --from=nginx-build /build/release/nginx /usr/sbin/nginx
+COPY --from=nginx-build /build/release/etc/nginx /etc/nginx
+COPY --from=nginx-build /build/release/etc/nginx /etc/nginx.sample
+
+RUN set -x \
+    && addgroup --system --gid 101 nginx \
+    && adduser --system --disabled-login --ingroup nginx --no-create-home --home /nonexistent --gecos "nginx user" --shell /bin/false --uid 101 nginx \
+    && apt update \
+    && apt install -y openssl \
+    && apt remove --purge --auto-remove -y \
+    && mkdir -p /var/log/nginx \
+    && ln -sf /dev/stdout /var/log/nginx/access.log \
+    && ln -sf /dev/stderr /var/log/nginx/error.log \
+# create a docker-entrypoint.d directory
+    && mkdir /docker-entrypoint.d
+
+COPY docker-entrypoint.sh /
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+EXPOSE 80
+EXPOSE 443
+
+STOPSIGNAL SIGTERM
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+# vim:sw=4:ts=4:et
+
+set -e
+
+if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    exec 3>&1
+else
+    exec 3>/dev/null
+fi
+
+if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
+    if [ ! `/usr/bin/find "/etc/nginx/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v` ]; then
+        echo >&3 "$0: No files found in /etc/nginx, create configure file"
+        cp -R /etc/nginx.sample/* /etc/nginx/ 2>/dev/null
+        mkdir -p /etc/nginx/ban-ip-access-cert 2>/dev/null
+        mkdir -p /etc/nginx/conf.d 2>/dev/null
+        
+# create ssl pem file
+        echo >&3 "$0: create /etc/nginx/dhparams.pem, please wait."
+        openssl dhparam -out /etc/nginx/dhparams.pem 2048
+        
+        echo >&3 "$0: create /etc/nginx/ban-ip-access-cert/default-key.pem, please wait."
+        openssl rand -writerand /root/.rnd
+        openssl genrsa -out /etc/nginx/ban-ip-access-cert/default-key.pem 2048
+        
+        echo >&3 "$0: create /etc/nginx/ban-ip-access-cert/default.pem, please wait."
+        openssl req -x509 -new -nodes -key /etc/nginx/ban-ip-access-cert/default-key.pem -days 36500 -out /etc/nginx/ban-ip-access-cert/default.pem -subj "/CN=default"
+
+        echo >&3 "$0: create pem file success."
+        
+        echo >&3 "$0: create configfile file, please wait."
+# create default config
+cat>/etc/nginx/ssl_params<<EOF
+ssl_session_cache         shared:SSL:10m;
+ssl_session_timeout       60m;
+
+ssl_session_tickets       on;
+# ssl_session_ticket_key   "ticket.key";
+
+ssl_dhparam               "dhparams.pem";
+
+ssl_protocols             TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+ssl_ciphers               TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+ECDSA+AES256:EECDH+ECDSA+3DES:EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:ECDHE-RSA-AES128-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA128:DHE-RSA-AES128-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA128:ECDHE-RSA-AES128-SHA384:ECDHE-RSA-AES128-SHA128:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA128:DHE-RSA-AES128-SHA128:DHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA384:AES128-GCM-SHA128:AES128-SHA128:AES128-SHA128:AES128-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4;
+ssl_prefer_server_ciphers on;
+EOF
+
+cat>/etc/nginx/nginx.conf<<EOF
+user                    www-data;
+
+worker_processes        auto;
+events {
+    worker_connections  1024;
+    # multi_accept on;
+}
+
+pid                     /var/run/nginx.pid;
+
+http {
+    # Basic Settings
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+
+    keepalive_timeout   60;
+    
+    types_hash_max_size 2048;
+    server_tokens      off;
+
+    # server_names_hash_bucket_size 64;
+    # server_name_in_redirect off;
+
+    include            /etc/nginx/mime.types;
+    default_type       application/octet-stream;
+
+    # Logging Settings
+    access_log         /var/log/nginx/access.log;
+    error_log          /var/log/nginx/errors.log;
+
+    # Gzip Settings
+    gzip               on;
+    gzip_vary          on;
+    
+    gzip_comp_level    6;
+    gzip_buffers       16 8k;
+
+    gzip_min_length    1000;
+    gzip_proxied       any;
+    gzip_disable       "msie6";
+
+    gzip_http_version  1.0;
+    gzip_types         text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript image/svg+xml;
+
+    # Brotli Settings
+    brotli             on;
+    brotli_comp_level  6;
+    brotli_types       text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript image/svg+xml;
+
+    # Ban IP access
+    include conf.d/ban-ip-access;
+
+    # Virtual Host Configs
+    include conf.d/*.conf;
+}
+
+stream {
+    # Stream Proxy Configs
+    include conf.d/*.stream;
+}
+EOF
+
+# ban-ip-access
+cat>/etc/nginx/conf.d/ban-ip-access<<EOF
+server {
+    listen                    80 default;
+    server_name               _;
+    
+    return                    444;
+}
+
+server {
+    listen                    443 ssl http2 default;
+    server_name               _;
+    
+    ssl_certificate           "ban-ip-access-cert/default.pem";
+    ssl_certificate_key       "ban-ip-access-cert/default-key.pem";
+    
+    include                   ssl_params;
+    return                    444;
+}
+EOF
+        echo >&3 "$0: create configfile file success."
+    fi
+    
+    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+        echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+        echo >&3 "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -n | while read -r f; do
+            case "$f" in
+                *.sh)
+                    if [ -x "$f" ]; then
+                        echo >&3 "$0: Launching $f";
+                        "$f"
+                    else
+                        # warn on shell scripts without exec bit
+                        echo >&3 "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *) echo >&3 "$0: Ignoring $f";;
+            esac
+        done
+
+        echo >&3 "$0: Configuration complete; ready for start up"
+    else
+        echo >&3 "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    fi
+fi
+
+exec "$@"
